5cb93d7a6cf5da33dcb6344e841ed0d846197219
Ams <adriansims62781@gmail.com>
2017-06-26 23:58:59 +0100

    first commit

---
+++ Async3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var x = new MyClass();
+                while (true)
+                {
+                    Console.WriteLine(x.Prop1);
+                    Thread.Sleep(500);
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
